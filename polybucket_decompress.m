0c52d5a891d330e87c7c052e985c3855d38e7d85
marcelostockle <91279708+marcelostockle@users.noreply.github.com>
2021-10-26 16:55:36 -0300

    [Normalize line endings] polybucket_decompress.m

--- polybucket_decompress.m
+++ polybucket_decompress.m
@@ -1,1 +1,24 @@
-function bucket = polybucket_decompress(query, period)    bucket = [];  if (query > 0)    bucket_len = 0;    aux = query;    while (aux > 0)      bucket_len++;      aux -= period^bucket_len;    endwhile        query = query - 1;    bucket = zeros(bucket_len, 1);    bucket_i = 1;    while (query > 0)      bucket(bucket_i) = mod(query, period);      query -= bucket(bucket_i);      query /= period;      query--;      bucket_i++;    endwhile    bucket++;  endif
+function bucket = polybucket_decompress(query, period)
+  
+  bucket = [];
+  if (query > 0)
+    bucket_len = 0;
+    aux = query;
+    while (aux > 0)
+      bucket_len++;
+      aux -= period^bucket_len;
+    endwhile
+    
+    query = query - 1;
+    bucket = zeros(bucket_len, 1);
+    bucket_i = 1;
+    while (query > 0)
+      bucket(bucket_i) = mod(query, period);
+      query -= bucket(bucket_i);
+      query /= period;
+      query--;
+      bucket_i++;
+    endwhile
+    bucket++;
+  endif
+  
