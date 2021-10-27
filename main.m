10845c01fba54b157dc79491786cb2769f950e1e
marcelostockle <91279708+marcelostockle@users.noreply.github.com>
2021-10-26 21:18:17 -0300

    [Fix] main.m
    Amended names used for function calls.

--- main.m
+++ main.m
@@ -15,9 +15,9 @@ seq = synth_random(args.symbols, args.len);
 [F1, count] = bpppf_f1(seq_d, args.symbols, args.period, args.min_support);
 
 mh_count = cell(args.period, 1);
-[mh_count{1}, hash_size] = minhash_hashing(seq_d, args.k, args.period, F1);
+[mh_count{1}, hash_size] = minhash_f1(seq_d, args.k, args.period, F1);
 
-[similarity, htable] = minhash_candidates(mh_count{1}, args.k, hash_size, args.period);
+[similarity, htable] = minhash_hashcount(mh_count{1}, args.k, hash_size, args.period);
 mh_count{2} = bpppf_f2(mh_count{1}, similarity, args.k, args.period, args.min_sim);
 
 exact_sim = exact_similarity(mh_count{2}, seq_d, args.period);
