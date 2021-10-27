2fc1752d457f15acfcba79b162094a11512d8581
marcelostockle <91279708+marcelostockle@users.noreply.github.com>
2021-10-26 21:07:42 -0300

    [Add] exact_similarity.m
    Brute-force calculation of exact association rule similarity values for all the patterns in a mh_count struct.

---
+++ exact_similarity.m
@@ -0,0 +1,23 @@
+function [similarity] =  exact_similarity(mh_count, seq, period)
+  cols = size(mh_count.hash, 2);
+  similarity = zeros(1, cols);
+  sim_or = zeros(1, cols);
+  sim_and = zeros(1, cols);
+  order = sum(mh_count.pattern(1,:) >= 0);
+  
+  windows = floor(numel(seq) / period);
+  aux_win = 1:period;
+  for (i = 1:windows)
+    win = aux_win + (i - 1) * period;
+    for (j = 1:cols)
+      match = sum(seq(win) == mh_count.pattern(j, :)');
+      if (match > 0)
+        sim_or(j)++;        
+        if (match == order);
+          sim_and(j)++;
+        endif
+      endif
+    endfor
+  endfor
+  
+  similarity = sim_and ./ sim_or
