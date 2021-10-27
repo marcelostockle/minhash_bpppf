165095f0dd54cff16e03ee7abe452dfaf083dc8f
marcelostockle <91279708+marcelostockle@users.noreply.github.com>
2021-10-27 20:05:49 -0300

    [Update] minhash_f1.m
    Adapt minhash_f1 to adopt max-heap priority queues instead of brute-force MinHash signature update.

--- minhash_f1.m
+++ minhash_f1.m
@@ -1,5 +1,6 @@
 function [mh_count, hash_size] = minhash_f1(seq, k, period, F1)
   
+  heap_order = 2;
   windows = floor(numel(seq) / period);
   hash_bits = 2 * nextpow2(windows);
   hash_size = pow2(hash_bits);
@@ -7,8 +8,6 @@ function [mh_count, hash_size] = minhash_f1(seq, k, period, F1)
   cols = size(F1, 1);
   mh_count = [];
   mh_count.hash = 2 * hash_size * ones(k, cols);
-  mh_count.maxind = ones(1, cols);
-  mh_count.maxhash = 2 * hash_size * ones(1, cols);
   mh_count.sig_len = zeros(1, cols);
   
   aux_win = 1:period;
@@ -20,10 +19,10 @@ function [mh_count, hash_size] = minhash_f1(seq, k, period, F1)
         if (mh_count.sig_len(i) < k)
           mh_count.sig_len(i)++;
           mh_count.hash(mh_count.sig_len(i), i) = h;
-          [mh_count.maxhash(i), mh_count.maxind(i)] = max(mh_count.hash(1:mh_count.sig_len(i), i));
-        elseif (h < mh_count.maxhash(i))
-          mh_count.hash(mh_count.maxind(i), i) = h;
-          [mh_count.maxhash(i), mh_count.maxind(i)] = max(mh_count.hash(:, i));
+          mh_count.hash(:, i) = maxheap_adjust(mh_count.hash(:, 1), mh_count.sig_len(i), heap_order);
+        elseif (h < mh_count.hash(1, i))
+          mh_count.hash(1, i) = h;
+          mh_count.hash(:, i) = maxheap_adjust(mh_count.hash(:, i), 1, heap_order);
         endif
       endif
     endfor
