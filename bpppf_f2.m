b9da8111fdd2314d8cd579c1c6f5c55f51216903
marcelostockle <91279708+marcelostockle@users.noreply.github.com>
2021-10-28 17:48:35 -0300

    [Update] bpppf_f2.m
    Updates bpppf_f2 to accommodate for the new max-heap signature structure. Signature comparison is performed using the HeapSort algorithm.

--- bpppf_f2.m
+++ bpppf_f2.m
@@ -1,8 +1,9 @@
 function [mh_count_f2] = bpppf_f2(mh_count, similarity, k, period, min_sim)
   
+  heap_order = 2;
   [I, J] = find(similarity >= min_sim);
   cols = numel(I);
-  mh_count_f2.hash = -1 * ones(k, cols);
+  mh_count_f2.hash = zeros(k, cols);
   mh_count_f2.sig_len = k * ones(1, cols);
   mh_count_f2.pattern = -1 * ones(cols, period);
   mh_count_f2.similarity = zeros(1, cols);
@@ -16,24 +17,35 @@ function [mh_count_f2] = bpppf_f2(mh_count, similarity, k, period, min_sim)
     non_comodin = pattern_b != -1;
     mh_count_f2.pattern(i, non_comodin) = pattern_b(non_comodin);
     
-    ind_a = 1;
-    ind_b = 1;
-    hash_a = mh_count.hash(ind_a, I(i));
-    hash_b = mh_count.hash(ind_b, J(i));
+    heap_new = -1 * ones(k, 1);
     sig_len = 0;
-    while (sig_len < k && ind_a <= k && ind_b <= k)
-      hash_a = mh_count.hash(ind_a, I(i));
-      hash_b = mh_count.hash(ind_b, J(i));
-      if (hash_a == hash_b)
+    heap_a = mh_count.hash(:, I(i));
+    heap_b = mh_count.hash(:, J(i));
+    heapend_a = mh_count.sig_len(I(i));
+    heapend_b = mh_count.sig_len(I(i));
+    while (sig_len < k && heapend_a > 0 && heapend_b > 0)
+      if (heap_a(1) == heap_b(1))
         sig_len++;
-        mh_count_f2.hash(sig_len, i) = hash_a;
-        ind_a++;
-        ind_b++;
-      elseif (hash_a < hash_b)
-        ind_a++;
+        heap_new(sig_len) = heap_b(1);
+        heap_new = maxheap_adjust(heap_new, sig_len, sig_len, heap_order);
+        
+        heap_a(1) = heap_a(heapend_a);
+        heapend_a--;
+        heap_a = maxheap_adjust(heap_a, heapend_a, 1, heap_order);
+        
+        heap_b(1) = heap_b(heapend_b);
+        heapend_b--;
+        heap_b = maxheap_adjust(heap_b, heapend_b, 1, heap_order);
+      elseif (heap_a(1) > heap_b(1))
+        heap_a(1) = heap_a(heapend_a);
+        heapend_a--;
+        heap_a = maxheap_adjust(heap_a, heapend_a, 1, heap_order);
       else
-        ind_b++;
+        heap_b(1) = heap_b(heapend_b);
+        heapend_b--;
+        heap_b = maxheap_adjust(heap_b, heapend_b, 1, heap_order);
       endif
     endwhile
+    mh_count_f2.hash(:, i) = heap_new;
     mh_count_f2.sig_len(i) = sig_len;
   endfor
