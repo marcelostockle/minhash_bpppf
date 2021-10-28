497fd28d1335284983a0e225d1f7b3cb162961c8
marcelostockle <91279708+marcelostockle@users.noreply.github.com>
2021-10-28 18:32:41 -0300

    [QoL update] main.m

--- main.m
+++ main.m
@@ -3,7 +3,7 @@
 args.len = 100000;
 args.symbols = 300;
 args.period = 5;
-args.support = 1000;
+% args.support = 1000;
 args.k = 100;
 args.min_support = 300 / args.len;
 args.min_sim = 0.01;
@@ -12,12 +12,19 @@ seq = synth_random(args.symbols, args.len);
 %% [seq_d, maxpattern]= synth_semideterm(args.symbols, seq, args.period, args.support);
 [seq_d, maxpattern]= synth_test(args.symbols, seq, args.period);
 
+disp("Finding F1...");
 [F1, count] = bpppf_f1(seq_d, args.symbols, args.period, args.min_support);
 
+disp("Hashing F1...");
 mh_count = cell(args.period, 1);
 [mh_count{1}, hash_size] = minhash_f1(seq_d, args.k, args.period, F1);
 
+disp("Hash count (F2)...");
 [similarity, htable] = minhash_hashcount(mh_count{1}, args.k, hash_size, args.period);
+
+disp("Hashing F2...");
 mh_count{2} = bpppf_f2(mh_count{1}, similarity, args.k, args.period, args.min_sim);
 
+disp("Finding exact F2 similarities...");
 [exact_sim, exact_supp] = exact_similarity(mh_count{2}, seq_d, args.period);
+disp("END");
